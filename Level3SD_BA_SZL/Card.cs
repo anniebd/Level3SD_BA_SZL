336714b77e4d39e86f7c9afa8e654daa9d28da4b
Anna Berghammer <anna.berghammer@stud.uni-corvinus.hu>
2021-11-05 18:57:03 +0100

    - pictures - card class

---
+++ Level3SD_BA_SZL/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Level3SD_BA_SZL
+{
+    class Card : PictureBox
+    {
+        public static int size = 80;
+        public Card()
+        {
+            Height = size;
+            Width = size;
+            Load($"Pictures/background.png");
+        }
+    }
+}
